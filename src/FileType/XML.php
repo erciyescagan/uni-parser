e037954396e96fa5d19125d1e6a7a76d09782dae
erciyescagan <erciyescagan@gmail.com>
2025-02-28 17:40:30 +0100

    Licensed by MIT

--- src/FileType/XML.php
+++ src/FileType/XML.php
@@ -1,5 +1,12 @@
 <?php
 
+/**
+ * UniParser - A PHP package for parsing various data files and generating SQL statements.
+ *
+ * @license MIT
+ * @copyright (c) [2025] [Mert Erciyes Çağan]
+ */
+
 
 namespace Merterciyescagan\UniParser\FileType;
 
